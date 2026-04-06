730042288824bc959e69b5090ff0f1890ff3211f
Feriel Zouak <ferielzouak@gmail.com>
2026-04-06 16:06:29 +0100

    adding new numbers

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@ void setup() {
 
 void loop() {
   digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
   digitalWrite(5, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, HIGH);
   digitalWrite(6, HIGH);
   digitalWrite(7, HIGH);
   digitalWrite(8, LOW);
