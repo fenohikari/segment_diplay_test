f1bebe1042dc71c6e42f3b11ed726f4cd5900dce
Feriel Zouak <ferielzouak@gmail.com>
2026-04-06 15:58:35 +0100

    saving the project

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+
+void setup() {
+  for (int i = 2; i < 10; i++) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+}
+
